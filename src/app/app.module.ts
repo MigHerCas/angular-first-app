ec4f31f8b8cbe424bd3542b42265ac13388902eb
Miguel Hernanz <migherdcas@gmail.com>
2020-10-05 13:05:14 +0200

    feat(SuccesAlert): CLI generated sample component (Assignment 1).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlert } from './WarningAlert/WarningAlert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent, WarningAlert],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
