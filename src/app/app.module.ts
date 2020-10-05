a88cf23e5ac92681b0a04dc73e4ef6484a09e9aa
Miguel Hernanz <migherdcas@gmail.com>
2020-10-05 12:58:18 +0200

    feat(WarningAlert): Creates sample component (assignment 1).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
+// Components
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlert } from './WarningAlert/WarningAlert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, WarningAlert],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
