4f8b4c8501ec3f152f7ee4ff122ea7d42ad6f8ac
Miguel Hernanz <migherdcas@gmail.com>
2020-10-05 13:02:47 +0200

    Last two naming fixes.

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-warningAlert',
+  selector: 'app-warning-alert',
   templateUrl: './warning-alert.component.html',
   styleUrls: ['./warning-alert.component.css'],
 })
